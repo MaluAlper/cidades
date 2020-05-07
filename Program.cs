7e9fc17e04385c95e15fb880084499c69ddafae8
Maria Luiza <malualper@hotmail.com>
2020-05-07 20:44:17 -0300

    cidades

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace cidades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cidades do Litoral Sul!");
+            Console.WriteLine("Mongaguá!");
+            Console.WriteLine("Itanhaém!");
+            Console.WriteLine("Peruibe!");
+        }
+    }
+}
